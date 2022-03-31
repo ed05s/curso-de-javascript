6a68bc1aad52aef4922047b882db2cfa316b15a6
Edinaldo Louredo Marques <edinaldolouredomarques@gmail.com>
2022-03-31 15:42:50 -0300

    exercício de condicionais em javasript aula 11

---
+++ aula011/ex008.js
@@ -0,0 +1,9 @@
+var vel = 12
+console.log(`a velocidade do seu carro é de ${vel} km/h`)
+
+if(vel>60){
+    console.log("multado")
+}
+
+
+console.log('dirija usando cinto de segurança')
---
+++ aula011/ex009.js
@@ -0,0 +1,9 @@
+var pais = "Brasil"
+console.log(`vivendo em ${pais}`)
+
+if(pais=="Brasil"){
+    console.log("Brasileiro")
+    
+}else{
+    console.log("Estrangeiro")
+}
