6874123699a8224a08542fe1bd903e3ecdaf7ea5
Edinaldo Louredo Marques <edinaldolouredomarques@gmail.com>
2022-04-05 15:41:22 -0300

    inclusão das linhas sobre condicional if

--- js-dev-aprender/fundamentos.js
+++ js-dev-aprender/fundamentos.js
@@ -1,10 +1,12 @@
-//{----ARRAYS----}
+//{----A R R A Y S----}
 // let puma = ['Gabriel','corno',5]
 
 // console.log(puma[1])
 
+//---------------------------------------------------------
 
-//{----FUNCTIONS----}
+
+//{----F U N C T I O N S----}
 //VERBO + SUBSTANTIVO
 
 // corSite = 'azul'
@@ -29,16 +31,20 @@
 // }
 //    console.log(x2(9))
 
+//---------------------------------------------------------
 
-//{----OPERADORES---}
-//ARITMÉTICOS
+
+//{----O P E R A D O R E S---}
+//ARITMÉTICOS 
 // ATRIBUÇÃO
 //COMPARAÇÃO
 //LÓGICOS
 //BITWISE
 //INCREMENTE E DECREMENTO
 
-//----ARITMÉTICOS  + , - , / , ** , *
+//---------------------------------------------------------
+
+//---- O P E R A D O R E S  A R I T M É T I C O S  + , - , / , ** , *
 // let salario=100
 
 //  console.log(salario+salario)
@@ -47,7 +53,9 @@
 //  console.log(salario*salario)
 //  console.log(5**5)
 
-//---- OPERADORE DE INCREMENTO E DECREMENTO
+//---------------------------------------------------------
+
+//(---- OPERADORE DE INCREMENTO E DECREMENTO----)
 // ++ E --
 
 //let idade=18
@@ -55,7 +63,9 @@
 // console.log(++idade) // atribui primeiro
 // console.log(idade++) // atribui depois
 
-//(----OPERADORES DE ATRIBUIÇÃO----) = , == , += , -= , == , ===
+//---------------------------------------------------------
+
+//(----O P E R A D O R E S  DE   A T R I B U I Ç ÃO----) = , == , += , -= , == , ===
 
 //let tecladoGamer=100
 //tecladoGamer = tecladoGamer+tecladoGamer
@@ -69,7 +79,7 @@
 //igualdade restrita verifica valor e tipo
 //console.log("1"===1)
 
-//(----OPERADOR TERNÁRIO----)
+//(----O P E R A D O R   T E R N Á R I O----)
 
 //TEM UM CLIENTE, 100 PPONTOS = PREMIUM/ COMUM
 
@@ -77,7 +87,9 @@
 //  let tipo = pontos>=100 ? "premium":"comum"
 //  console.log(tipo)
 
-//(----OPERADORES LÓGICOS----)
+//---------------------------------------------------------
+
+//(----O P E R A D O R E S   L Ó G I C O S----)
 
 //OPERADOR (&&) E = RETORNA TRUE SE duas CONDIÇÕES FOREM true
 
@@ -97,5 +109,52 @@
 
 // let candidatoRecusado = !podeAplicar
 // console.log( "não pode aplicar ",candidatoRecusado)
+
+//---------------------------------------------------------
+
+
+//(----O P E R A D O R E S   NÃO  B O O L E A NOS----)
+
+// FALSY 
+ // undefined
+ //null
+ //0
+ //falso
+ //''
+ //Nan not a number
+
+ // TRUTHY 
+  // true
+
+//   let corPersonalizada = ''
+//   let corPadrao = 'azul'
+//   let corPerfil = corPersonalizada || corPadrao
+
+//   console.log(corPerfil)
+
+//---------------------------------------------------------
+
+
+
+//{----E S T R U T U R A S   D E   R E P E T I Ç Ã O----}
+
+// IF..ELSE
+//SWITCH...CASE
+
+// SE A HORA ESTIVER ENTRE 06:00 ATE 12:00 : BOM DIA
+// SE A HORA ESTIVER ENTRE 12:00 ATE 18:00 : BOA TARDE
+// CASO O CONTRÁRIO : BOA NOITE
+
+// let hora = 10
+//  if(hora<12){
+//      console.log("bom dia")
+//  }else if(hora>12 && hora<=18){
+//      console.log("boa tarde")    
+//  }else{
+//      console.log('boa noite')
+//  }
+
+  
+
 
 
