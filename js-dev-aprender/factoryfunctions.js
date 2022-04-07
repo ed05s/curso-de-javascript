356f5be35b096f9d6f4520e98047b8f0fa7bfba5
Edinaldo Louredo Marques <edinaldolouredomarques@gmail.com>
2022-04-07 17:27:13 -0300

    constructor function adicionado aos exercícios

--- js-dev-aprender/factoryfunctions.js
+++ js-dev-aprender/factoryfunctions.js
@@ -1,30 +1,44 @@
-const celular = {
-    marcaCelular:'asus',
-    tamanhoTela:{
-        vertical:155,
-        horizontal:75
-    },
-    capacidadedeBateria:5000,
-    ligar : function(){
-        console.log('fazendo Ligação')
-    }
+// const celular = {
+//     marcaCelular:'asus',
+//     tamanhoTela:{
+//         vertical:155,
+//         horizontal:75
+//     },
+//     capacidadedeBateria:5000,
+//     ligar : function(){
+//         console.log('fazendo Ligação')
+//     }
 
-}
+// }
 // factory function
 
 // criando um construtor
-function criarCelular(marcaCelular, tamanhoTela,capacidadedeBateria){
-    return {
-        marcaCelular,
-        tamanhoTela,   
-        capacidadedeBateria,
-        ligar(){
-            console.log('fazendo Ligação')
-        }
+// function criarCelular(marcaCelular, tamanhoTela,capacidadedeBateria){
+//     return {
+//         marcaCelular,
+//         tamanhoTela,   
+//         capacidadedeBateria,
+//         ligar(){
+//             console.log('fazendo Ligação')
+//         }
     
-    }
+//     }
     
 
-}
- const celular1=criarCelular('zenfone',5.5,5000)
- console.log(celular1)
+// }
+//  const celular1=criarCelular('zenfone',5.5,5000)
+//  console.log(celular1)
+
+ // constructor function
+ //pascal case - UmDoisTresQuatro
+
+//  function Celular(marcaCelular,tamanhoTela,capacidadedeBateria,){
+//      this.marcaCelular = marcaCelular,
+//      this.tamanhoTela =  this.tamanhoTela,
+//      this.capacidadedeBateria = this.capacidadedeBateria,
+//      this.ligar=function(){
+//          console.log("fazendo ligação")
+//      }
+//  }
+//  const celular = new Celular('asus',5,5,5000)
+//  console.log(celular)
