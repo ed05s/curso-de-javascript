89be2d107dcacc5d6d3cc0ba0d6f2bfad9bc6bc5
Edinaldo Louredo Marques <edinaldolouredomarques@gmail.com>
2022-04-07 17:15:47 -0300

    exercício de factory function adicionado nos exercícios

--- js-dev-aprender/exercícios.js
+++ js-dev-aprender/exercícios.js
@@ -2,14 +2,34 @@
 //FUNCTION
 let sapato= "festa"
 
-function mudarSapato(tipo,cor){
-   sapato = tipo + cor
- } 
+// function mudarSapato(tipo,cor){
+//    sapato = tipo + cor
+//  } 
  
-mudarSapato("esporte ","laranja")
-console.log(sapato)
+// mudarSapato("esporte ","laranja")
+// console.log(sapato)
 
-//FUNCTION SEM RETO
+//--------------------------------
+
+// factory functions
+
+// const filme ={titulo:'robocop'
+//               ,Genero:'ação'
+//               ,estudio:'sony'
+//              ,ano:20 }
+// console.log(filme)
+
+// function criarFilme(titulo,Genero,estudio,ano){
+//   return{
+//     titulo,
+//     Genero,
+//     estudio,
+//     ano
+//   }
+
+// }
+// const filme1= criarFilme('vingadores','ficção','marvel studios',2018)
+// console.log(filme1)
 
 
 
