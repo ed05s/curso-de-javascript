48df2aebc5c213e42941d58e1c396f297c468c68
Edinaldo Louredo Marques <edinaldolouredomarques@gmail.com>
2022-04-07 16:50:41 -0300

    numeros primos

---
+++ js-dev-aprender/miniprojeto010.js
@@ -0,0 +1,23 @@
+// criar função para mostrar on números primos
+
+//primos
+//compostos
+exibirNumerosPrimos(15)
+
+function exibirNumerosPrimos(limite){
+        for(numero=2;numero<=limite;numero++){
+             
+                 if(numeroPrimo(numero))console.log(numero)
+       }
+
+}
+function numeroPrimo(numero){
+             
+    for(let divisor=2; divisor<numero;divisor++){
+        if(numero%divisor===0){
+              return false
+         
+        }
+    }
+    return true
+}
